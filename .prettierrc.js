c9181e88e462ab454b55dc607c9f37e0ba73d48c
Vinicius Rocha <vinirocha05@gmail.com>
2022-07-28 08:14:22 -0300

    config styled components

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   semi: true,
   trailingComa: 'all',
-  sigleQuote: true,
+  singleQuote: true,
   printWidth: 80,
   tabWidth: 2
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import { AppProps } from "../../node_modules/next/dist/shared/lib/router/router";
+import { GlobalStyles } from "../styles/global-styles";
+import { theme } from "../styles/theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
