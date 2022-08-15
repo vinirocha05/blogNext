dbc064e06237907c9d4195aa5ab8ccabd5776f49
Vinicius Rocha <vinirocha05@gmail.com>
2022-08-14 21:44:29 -0300

    adjusting to fit prettir presests agin

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -5,7 +5,9 @@ import { customRender } from '../../utils/custom-render';
 describe('<Heading />', () => {
   it('should render a heading', () => {
     customRender(<Heading>Oi</Heading>);
-    const element = screen.getByRole('heading', { name: /oi/i });
+    const element = screen.getByRole('heading', {
+      name: /oi/i,
+    });
     expect(element).toHaveStyleRule('font-size', '5rem');
   });
 });
--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,7 +1,9 @@
-export const API_URL = 'https://pure-hamlet-67700.herokuapp.com/api/';
+export const API_URL =
+  'https://pure-hamlet-67700.herokuapp.com/api/';
 
 export const POSTS_URL =
   'https://pure-hamlet-67700.herokuapp.com/api/posts?populate=*';
 
 export const SITE_NAME = 'Vini Rocha';
-export const SITE_URL = 'https://blog-next-o5ms6el9e-vinirocha05.vercel.app/';
+export const SITE_URL =
+  'https://blog-next-o5ms6el9e-vinirocha05.vercel.app/';
